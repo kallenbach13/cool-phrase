ab5b75855668b19bc29f2b90b52ebcc4e68376dd
Isak Kallenbach <kallenbach13@gmail.com>
2022-02-18 18:08:46 +0000

    added punctuation array and random punctuation functionality

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const coolWords = {
     verb: ["Feeling'", "Lookin'", "Smellin'", "Vibin'", "Ridin'"],
     adj: ["hard", "sweet", "high", "tubular", "bossanova", "jazzy", "wicked", "sick", "slick"],
-    noun: ["my guy", "lady", "dude", "bro", "sis", "cuz", "fam", "buddy"]
+    noun: ["my guy", "lady", "dude", "bro", "sis", "cuz", "fam", "buddy"],
+    punct: ["?", "!", ".", "!?", "!!!", "..."]
 };
-
-let randomVerb = coolWords.verb[Math.floor(Math.random() * coolWords.verb.length)];
-let randomAdj = coolWords.adj[Math.floor(Math.random() * coolWords.adj.length)];
-let randomNoun = coolWords.noun[Math.floor(Math.random() * coolWords.noun.length)];
 
 
 const CoolPhrase = () => {
-
+    let randomVerb = coolWords.verb[Math.floor(Math.random() * coolWords.verb.length)];
+    let randomAdj = coolWords.adj[Math.floor(Math.random() * coolWords.adj.length)];
+    let randomNoun = coolWords.noun[Math.floor(Math.random() * coolWords.noun.length)];
+    let randomPunct = coolWords.punct[Math.floor(Math.random() * coolWords.punct.length)];
+    
+    return `${randomVerb} ${randomAdj}, ${randomNoun}${randomPunct}`;
 };
+
+CoolPhrase()
