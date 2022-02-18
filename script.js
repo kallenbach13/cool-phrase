b44f4bf18f148381045177fcfdf21b76cc8a5016
Isak Kallenbach <kallenbach13@gmail.com>
2022-02-18 17:53:08 +0000

    adds message storage object

---
+++ script.js
@@ -0,0 +1,9 @@
+const coolWords = {
+    verb: ["Feeling'", "Lookin'", "Smellin'", "Vibin'", "Ridin'"],
+    adjective: ["hard", "sweet", "high", "tubular", "bossanova", "jazzy", "wicked", "sick", "slick"],
+    noun: ["my guy", "lady", "dude", "bro", "sis", "cuz", "fam",]
+};
+
+const CoolTool = () => {
+
+};
